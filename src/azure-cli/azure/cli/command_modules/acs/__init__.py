1d31774e4f0982bd67414a8a9ab75ae2cc2bb364
nvkhacker <63601005+nvkhacker@users.noreply.github.com>
2025-12-09 15:23:54 +0530

    Add POC comment in __init__.py
    Added a comment indicating POC by @nvk0x.

--- src/azure-cli/azure/cli/command_modules/acs/__init__.py
+++ src/azure-cli/azure/cli/command_modules/acs/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-
+# POC by @nvk0x
 
 from azure.cli.core import AzCommandsLoader
 
